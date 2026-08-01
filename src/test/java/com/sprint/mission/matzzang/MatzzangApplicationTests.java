cb4abc2e9cb4fcd392c1b7a7fc0ae2ca98524e67
Seongkyeong Kim <gimmy6707@gmail.com>
2026-08-01 17:55:44 +0900

    Feat: 레포지토리 초기 설정 및 CI 워크플로우 추가
    # Conflicts:
    #	.gitignore
    #	src/test/java/com/sprint/mission/matzzang/MatzzangApplicationTests.java

--- src/test/java/com/sprint/mission/matzzang/MatzzangApplicationTests.java
+++ src/test/java/com/sprint/mission/matzzang/MatzzangApplicationTests.java
@@ -3,7 +3,7 @@ package com.sprint.mission.matzzang;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class MatzzangApplicationTests {
 
     @Test
