00f701e641e08419e25db1a2176df7d3e3a1f20b
Seongkyeong Kim <gimmy6707@gmail.com>
2026-08-20 14:49:00 +0900

    Chore: 주석 추가

--- src/main/java/com/sprint/mission/matzzang/user/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/matzzang/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sprint.mission.matzzang.user.repository;
 
 import com.sprint.mission.matzzang.user.entity.User;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,10 +11,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // 입력받은 email로 가입된 유저가 있는지?
     boolean existsByEmail(String email);
 
+    // 입력받은 유저 이름으로 가입된 유저가 있는지?
     boolean existsByUsername(String username);
 
+    // 커서와 Pageable 을 입력받고, 해당 커서 이후의 유저 목록들을 뽑아온다.
     @Query("SELECT u FROM User u WHERE :cursor IS NULL OR u.id > :cursor ORDER BY u.id ASC")
     List<User> findAllByCursor(@Param("cursor") Long cursor, Pageable pageable);
 }
